3215b5aee88b1081693e3be94fed00386ee1407d
ArdakHere <mrglok1@gmail.com>
2025-07-28 11:46:41 +0500

    added more description to the endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, Form, BackgroundTasks
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from services.core import make_request_normalize_excel_driver
 import os 
 
@@ -15,8 +15,16 @@ async def root():
 async def normalize_excel(
     background_tasks: BackgroundTasks,
     file: UploadFile = File(...),
-    column_name: str = Form(...),
-    prompt_str: str = Form(...)
+    название_колонки: str = Form(
+        ...,
+        description="Введите название целевой колонки, которую нужно нормализовать",
+        example=" "
+    ), 
+    инструкция: str = Form(
+        ...,
+        description="Введите промпт-инструкцию",
+        example=" "
+    )
 ):
     temp_filename = f"temp_{file.filename}"
 
@@ -24,7 +32,7 @@ async def normalize_excel(
         f.write(await file.read())
 
     # ✅ Get path to processed file
-    output_file = await make_request_normalize_excel_driver(temp_filename, column_name, prompt_str)
+    output_file = await make_request_normalize_excel_driver(temp_filename, название_колонки, инструкция)
 
     # Optional cleanup (delete temp and output file after sending)
     background_tasks.add_task(os.remove, temp_filename)
